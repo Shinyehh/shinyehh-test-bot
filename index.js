31e0d6f03a17f39a3452ae484540f1e2dac10041
Shinyehh <shinypikachu1234@gmail.com>
2022-11-25 23:23:17 -0800

    part 2

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
-    intents: [
+   /* intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
     ]
+    */
+   intents: [
+    "Guilds",
+    "GuildMessages",
+    "MessageContent",
+    "GuildMembers"
+   ]
 })
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`);
+    console.log(`WELCOME! Logged in as ${client.user.tag}`);
 })
 
-client.on("messageCreate", (message) => {
-    console.log(message);
+const welcomeChannelId = "1045917175459876914"
+
+client.on("messageCreate", async (message) => {
     if (message.content == "hi"){
         message.reply("Hello World!");
+    } else if (message.content == "image"){
+        const member = message.author
+        const img = await generateImage(member)
+        const welcomeChannel = message.guild.channels.cache.get(welcomeChannelId)
+        
+        console.log([img])
+        welcomeChannel.send({
+            content: `<@${member.id}> Welcome to the server!`,
+            files: [img]
+        })
     }
+})
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member.user)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
